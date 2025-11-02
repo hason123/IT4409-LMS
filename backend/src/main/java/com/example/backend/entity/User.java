aef13ca7ada99173827448395a74c0c4c272c089
hason123 <hason051004@gmail.com>
2025-11-02 22:25:27 +0700

    change User attributes

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -1,15 +1,12 @@
 package com.example.backend.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
-import java.time.LocalDate;
 import java.util.List;
 
 @Setter
@@ -17,14 +14,14 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name= "users")
+@Table(name= "user")
 @SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE user_id = ?")
 @SQLRestriction(value = "is_deleted = false") //mac dinh chi lay nhung ban gi ko bi soft delete
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long userId;
+    private Long id;
     @Column(name = "user_name", unique = true)
     private String userName;
     @Column(name = "pass_word")
@@ -33,18 +30,24 @@ public class User extends BaseEntity {
     private String fullName;
     @Column(name = "phone_number", unique = true)
     private String phoneNumber;
+    @Column(name = "birthday")
+    private String bỉrthday;
     @Column(name = "student_number", unique = true)
     private String studentNumber;
-    @Column(name = "birthday")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalDate birthday;
     @Column(name = "address")
     private String address;
     @Column(name = "refresh_token", columnDefinition = "MEDIUMTEXT")
     private String refreshToken;
+    @Column(name = "gmail")
+    private String gmail;
     @ManyToOne
-    @JoinColumn(name = "user_role_id")
+    @JoinColumn(name = "role_id")
     private Role role;
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+    @OneToMany(mappedBy = "teacher")
+    private List<Course> taughtCourses;
 
 
 }
