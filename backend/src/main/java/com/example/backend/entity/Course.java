238366b0933a9d0d68b80e7f9a15d1b1a2a49ae3
quyenaoy <aoynehi@gmail.com>
2025-11-03 00:17:27 +0700

    Add Course.java entity class

---
+++ backend/src/main/java/com/example/backend/entity/Course.java
@@ -0,0 +1,32 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "course")
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE user_id = ?")
+@SQLRestriction(value = "is_deleted = false")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    @OneToMany(mappedBy = "course")
+    private List<Chapter> chapters;
+    @OneToMany(mappedBy = "course")
+    private List<User> students;
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private User teacher;
+
+
+}
