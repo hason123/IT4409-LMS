9db644a3254c4b0037e09d5083a3bd39a165c510
sonkila <sonkila1324@gmail.com>
2025-11-27 11:37:45 +0700

    add login by google

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import AuthPage from './pages/AuthPage'
 import Home from './pages/Home'
 import CoursesPage from './pages/CoursesPage'
 import CourseDetailPage from './pages/CourseDetailPage'
 
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID_HERE';
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<AuthPage defaultTab="login" />} />
-      <Route path="/register" element={<AuthPage defaultTab="register" />} />
-      <Route path='/courses' element={<CoursesPage />} />
-      <Route path='/courses/:id' element={<CourseDetailPage />} />
-    </Routes>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<AuthPage defaultTab="login" />} />
+        <Route path="/register" element={<AuthPage defaultTab="register" />} />
+        <Route path='/courses' element={<CoursesPage />} />
+        <Route path='/courses/:id' element={<CourseDetailPage />} />
+      </Routes>
+    </GoogleOAuthProvider>
   )
 }
