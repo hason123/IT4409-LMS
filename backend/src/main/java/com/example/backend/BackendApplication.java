0957073efbdc3e9c406f41958408935ae62a9aac
hason123 <hason051004@gmail.com>
2026-01-13 02:44:36 +0700

    fix docker

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -10,13 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
+       /* Dotenv dotenv = Dotenv.configure()
                 .directory("./")
                 .filename(".env")
                 .load();
         dotenv.entries().forEach(entry -> {
             System.setProperty(entry.getKey(), entry.getValue());
-        });
+        });*/
         SpringApplication.run(BackendApplication.class, args);
     }
 
